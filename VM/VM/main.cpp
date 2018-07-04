7a280030a09568737fc60277ecf5505a2e4ee328
Hunter Rasmussen <hunterhrasmussen@gmail.com>
2018-07-04 10:47:51 -0600

    Started VM project.

---
+++ VM/VM/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  VM
+//
+//  Created by Hunter Rasmussen on 6/30/18.
+//  Copyright © 2018 Hunter Rasmussen. All rights reserved.
+//
+
+#include <iostream>
+#include <stdint.h>
+
+const int   INSTRUCTION_SIZE = 16;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+
+    uint16_t memory[INSTRUCTION_SIZE];
+    uint16_t registers[];
+    
+    
+    return 0;
+}
