f1b5c24c87171b41923ebbb16b5d1713e1f2a60e
Miranda Mason <mimi@Mirandas-MacBook-Pro.local>
2018-07-06 17:13:45 -0600

    Added enum for ops and started building functions for executing instructions

--- VM/VM/main.cpp
+++ VM/VM/main.cpp
@@ -10,13 +10,32 @@
 #include <stdint.h>
 
 const int   INSTRUCTION_SIZE = 16;
+enum operators {ADD, AND, BR, JMP, RET, JSR, JSRR,
+                LD, LDI, LDR, LEA, NOT, RTI, ST,
+                STI, STR, TRAP,
+};
+
+uint16_t executeInstruction(uint16_t instruction){
+    
+    
+    return NULL;
+}
+
+
+
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
+    uint16_t instructions[24]; //TODO
+    for(auto instruction : instructions){
+        executeInstruction(instruction);
+    }
+    
     std::cout << "Hello, World!\n";
 
     uint16_t memory[INSTRUCTION_SIZE];
-    uint16_t registers[];
+    //uint16_t registers[];
     
     
     return 0;
