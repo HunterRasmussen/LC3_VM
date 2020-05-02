bf9494549218ade3d8c7238c9a7ba3ca44c55319
Hunter <hunterhrasmussen@gmail.com>
2020-05-02 10:35:49 -0600

    Update before computer restore

--- VM/VM/main.cpp
+++ VM/VM/main.cpp
@@ -7,7 +7,18 @@
 //
 
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/termios.h>
+#include <sys/mman.h>
 
 const int   MEM_SIZE = UINT16_MAX;
 uint16_t memory[MEM_SIZE];
@@ -116,6 +127,38 @@ void updateFlags(uint16_t drIndex){
     }
 }
 
+struct termios original_tio;
+
+void disable_input_buffering()
+{
+    tcgetattr(STDIN_FILENO, &original_tio);
+    struct termios new_tio = original_tio;
+    new_tio.c_lflag &= ~ICANON & ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+}
+
+void restore_input_buffering()
+{
+    tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
+}
+
+void handle_interrupt(int signal) {
+    restore_input_buffering();
+    printf("\n");
+    exit(-2);
+}
+
+uint16_t check_key() {
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+    
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 0;
+    return select(1, &readfds, NULL, NULL, &timeout) != 0;
+}
+
 uint16_t swap16(uint16_t x) { return (x << 8) | (x >> 8); }
 
 void read_image(FILE* imageToRead){
@@ -134,6 +177,7 @@ void read_image(FILE* imageToRead){
 
 int main(int argc, const char * argv[]) {
     
+    disable_input_buffering();
 
     if(argc > 1){
         FILE* imageToRead = fopen(argv[1], "r");  //read only
